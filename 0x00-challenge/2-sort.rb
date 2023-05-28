5d9329c3fbb6d0ca08bd7154b775e2f96972364c
Ocholi Joseph <ocholijoseph@gmail.com>
2023-05-28 01:32:06 -0700

    Sorting as in is done in Ruby

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,31 +1,24 @@
 ###
+##
+##  Sort integer arguments (ascending)
+##
+####
 #
-#  Sort integer arguments (ascending) 
+#result = []
+#ARGV.each do |arg|
+#    # skip if not integer
+#        next if arg !~ /^-?[0-9]+$/
 #
-###
-
-result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
-
-    # convert to integer
-    i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
-    end
-    result << i_arg if !is_inserted
-end
-
-puts result
+#            # convert to integer
+#                i_arg = arg.to_i
+#
+#                        # insert result at the right position
+#                            i = 0
+#                                while i < result.size && result[i] < i_arg
+#                                        i += 1
+#                                            end
+#                                                result.insert(i, i_arg)
+#                                                end
+#
+#                                                puts result
+#
