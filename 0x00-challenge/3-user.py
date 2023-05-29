b0b3698ba618e87179452c2bc45125a94359e7db
Ocholi Joseph <ocholijoseph@gmail.com>
2023-05-28 20:22:22 -0700

    Challenge on

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -42,8 +42,6 @@ class User():
         else:
             self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
-
-
     def is_valid_password(self, pwd):
         """
         Valid password:
@@ -54,9 +52,9 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        if self.password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
